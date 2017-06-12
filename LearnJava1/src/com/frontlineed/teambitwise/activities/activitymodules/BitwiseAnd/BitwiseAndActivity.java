ae43f3723518a990912b87b5a79526e88ca15080
Robert Hackman <rhackman@frontlineed.com>
2017-06-12 15:56:15 -0400

    Checking in AnimalRace v1

--- LearnJava1/src/com/frontlineed/teambitwise/activities/activitymodules/BitwiseAnd/BitwiseAndActivity.java
+++ LearnJava1/src/com/frontlineed/teambitwise/activities/activitymodules/BitwiseAnd/BitwiseAndActivity.java
@@ -12,8 +12,8 @@ import java.util.List;
  */
 public class BitwiseAndActivity implements IActivity
 {
-    public String title = "Bitwise And";
-    public String description = "Guess the result of the Bitwise And operation(s) below.";
+    public String title = "Animal Race";
+    public String description = "Multi-threading and inheritance demo";
     public List<QuestionAndAnswer> questionList = new ArrayList<QuestionAndAnswer>();
     public Integer score = 0;
 
@@ -22,20 +22,60 @@ public class BitwiseAndActivity implements IActivity
     {
         System.out.println("'" + title + "' Activity is starting...");
 
-        QuestionAndAnswer<YesNoType> q1 = new QuestionAndAnswer<>(YesNoType.YES,"Is 2 & 3 == 2?");
-        q1.ask();
-        q1.getUserInput();
-        q1.parseUserInput(YesNoType.class);
+        int raceDistance = 50; // 100 feet
+        int tortoiseRunDistance = 1; // 1 foot per second
+        long tortoiseRunTime = 1; // 1 second
+        int tortoiseSleepTime = 1; // 1 second
+        int hareRunDistance = 5; // 5 feet per second
+        long hareRunTime = 1; // 1 second
+        int hareSleepTime = 10; // 10 seconds
 
-        if(q1.userAnsweredCorrectly())
-        {
-            System.out.println("Correct! Good job");
-        }
-        else
-        {
-            System.out.println("Sorry! Incorrect");
-        }
+        long raceSpeed = 200; //milliseconds
 
+        // Start race
+        Runnable tortoiseRun = () -> {
+            int tortoiseCurrentLocation = 0;
+            while(tortoiseCurrentLocation < raceDistance) {
+                System.out.println("Tortoise is at location " + tortoiseCurrentLocation);
+                tortoiseCurrentLocation = tortoiseCurrentLocation + tortoiseRunDistance;
+
+                try {
+                    Thread.sleep(tortoiseRunTime*raceSpeed);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("Tortoise is at location " + tortoiseCurrentLocation + ". Tortoise is done!");
+        };
+
+        Runnable hareRun = () -> {
+            int hareCurrentLocation = 0;
+            while(hareCurrentLocation < raceDistance) {
+                System.out.println("Hare is at location " + hareCurrentLocation);
+                hareCurrentLocation = hareCurrentLocation + hareRunDistance;
+
+                try {
+                    Thread.sleep(hareRunTime*raceSpeed);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("Hare is at location " + hareCurrentLocation + ". Hare is done!");
+        };
+
+        Thread t1 = new Thread(tortoiseRun);
+        Thread t2 = new Thread(hareRun);
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("'" + title + "' Activity is ending...");
     }
 }
